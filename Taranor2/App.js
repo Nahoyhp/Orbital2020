16cb85bf62e9d35c6f8b12ef3baafc1943bda422
hengjeejoo <65456021+hengjeejoo@users.noreply.github.com>
2020-05-26 13:09:51 +0800

    move folder

---
+++ Taranor2/App.js
@@ -0,0 +1,68 @@
+// App.js
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Login from './components/login';
+import Signup from './components/signup';
+import Dashboard from './components/dashboard';
+import addModule from './components/addModuleList'
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="AddModule" //try dashboard first to test button for add module
+      screenOptions={{
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#3740FE',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      <Stack.Screen 
+        name="Signup" 
+        component={Signup} 
+        options={{ title: 'Signup' }}
+      />       
+      <Stack.Screen 
+        name="Login" 
+        component={Login} 
+        options={
+          {title: 'Login'},
+          {headerLeft: null} 
+        }
+      />
+      <Stack.Screen 
+       name="Dashboard" 
+       component={Dashboard} 
+       options={
+         { title: 'Dashboard' },
+         {headerLeft: null} 
+       }
+      />
+      
+      <Stack.Screen 
+       name="AddModule" 
+       component={addModule} 
+       options={
+         { title: 'AddModule' },
+         {headerLeft: null} 
+       }
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
---
+++ Taranor2/database/config.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCt5SFbaeVIjcALY5SynzKVKt8qzEzRfg0",
+    authDomain: "taranorv2.firebaseapp.com",
+    databaseURL: "https://taranorv2.firebaseio.com",
+    projectId: "taranorv2",
+    storageBucket: "taranorv2.appspot.com",
+    messagingSenderId: "361539483136",
+    appId: "1:361539483136:web:f5f13b1a1a2e6d6c0a8ae5",
+}
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+export default firebase;
